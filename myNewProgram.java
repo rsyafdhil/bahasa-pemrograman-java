e00ed8e4d17523a049c5e654e5352f6408dca6ae
Rassya <rasyafadhil@gmail.com>
2025-09-07 20:46:11 +0700

    Tugas Teori Done

--- myNewProgram.java
+++ myNewProgram.java
@@ -1,30 +1,90 @@
+import java.lang.reflect.Method;
 import java.util.*;
+import javax.swing.JOptionPane;
+
+class Mahasiswa {
+    String nama;
+
+    public Mahasiswa(String nama) {
+        this.nama = nama;
+    }
+}
 
 public class myNewProgram {
-
     public static void main(String[] args) {
-        // int ages[] = new int[5];
-        // ages[0] = 35;
-        // System.out.println(Arrays.toString(ages));
+        ArrayList<Mahasiswa> daftarMahasiswa = new ArrayList<>();
 
-        final int ROWS = 2;
-        final int COLS = 3;
-        int grades[][] = new int[ROWS][COLS];
-        grades[0][0] = 100;
-        grades[0][1] = 90;
-        grades[0][2] = 80;
-        grades[1][0] = 70;
-        grades[1][1] = 60;
-        grades[1][2] = 50;
+        String operasi;
+        do {
+            operasi = JOptionPane.showInputDialog(null,
+                    "Masukkan Operasi yang mau dilakukan:\n1. Tambah Mahasiswa\n2. Menghapus Data Mahasiswa\n3. Mengubah Data Mahasiswa\n4. Mengurutkan Data Mahasiswa\n5. Menampilkan Data Mahasiswa\n");
 
-        for (int rows = 0; rows < ROWS; rows++) {
-            for (int cols = 0; cols < COLS; cols++) {
-                System.out.println("Rows: " + rows + " Cols: " + cols + " Value: " + grades[rows][cols]);
+            switch (operasi) {
+                case "1": // Tambah mhs
+                    String tambahNama = JOptionPane
+                            .showInputDialog(operasi + ". Masukkan Nama Mahasiswa yang mau ditambahkan:");
+                    daftarMahasiswa.add(new Mahasiswa(tambahNama));
+                    break;
+                case "2": // Hapus mhs
+                    String hapusNama = JOptionPane
+                            .showInputDialog(operasi + ". Masukkan nama mahasiswa yang mau dihapus: ");
+                    boolean isDeleted = false;
+                    Iterator<Mahasiswa> iterator = daftarMahasiswa.iterator();
+                    while (isDeleted == false && iterator.hasNext()) {
+                        Mahasiswa mhs = iterator.next();
+                        if (mhs.nama.equals(hapusNama)) {
+                            iterator.remove();
+                            isDeleted = true;
+                        }
+                    }
+                    if (isDeleted) {
+                        System.out.println("Data mahasiswa " + hapusNama + " berhasil dihapus.");
+                    } else {
+                        System.out.println("Data mahasiswa " + hapusNama + " tidak ditemukan.");
+                    }
+                    break;
+                case "3": // edit hms
+                    String cariNama = JOptionPane
+                            .showInputDialog(operasi + ". Masukkan nama mahasiswa yang mau diubah: ");
+                    boolean isEdited = false;
+                    ListIterator<Mahasiswa> listIterator = daftarMahasiswa.listIterator();
+                    while (isEdited == false && listIterator.hasNext()) {
+                        Mahasiswa mhs = listIterator.next();
+                        if (mhs.nama.equals(cariNama)) {
+                            String namaBaru = JOptionPane
+                                    .showInputDialog(mhs.nama + " ditemukan. Masukkan nama baru : ");
+                            mhs.nama = namaBaru;
+                            isEdited = true;
+                        }
+                    }
+                    if (isEdited) {
+                        System.out.println("Data mahasiswa " + cariNama + " berhasil diubah. ");
+
+                    } else {
+                        System.out.println("Data mahasiswa " + cariNama + " tidak ditemukan.");
+                    }
+                    break;
+                case "4": // urutkan mhs
+                    Collections.sort(daftarMahasiswa, new Comparator<Mahasiswa>() {
+                        @Override
+                        public int compare(Mahasiswa m1, Mahasiswa m2) {
+                            return m1.nama.compareTo(m2.nama);
+                        }
+                    });
+                    System.out.println("Data mahasiswa berhasil diurutkan secara alfabet.");
+                    break;
+                case "5":
+                    StringBuilder daftar = new StringBuilder();
+                    for (Mahasiswa mhs : daftarMahasiswa) {
+                        daftar.append(mhs.nama).append("\n");
+                    }
+                    JOptionPane.showMessageDialog(null, daftar.toString(), "Daftar Mahasiswa",
+                            JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                default:
+                    break;
             }
-            System.out.println();
-        }
-
-        // System.out.println(Arrays.toString(grades[1]));
-
+        } while (!operasi.equals("6"));
+        System.out.println(daftarMahasiswa);
     }
 }
